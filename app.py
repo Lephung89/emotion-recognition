29a829eb283216d5316705efc1b844069832493b
Lephung89 <162795293+Lephung89@users.noreply.github.com>
2025-06-11 14:04:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Constants
-MODEL_PATH = "inception_emotion_model.keras"
-GOOGLE_DRIVE_URL = "https://drive.google.com/file/d/1okZqT58Ic9-i7J3kbZntT_ftwnK-kCyB/view?usp=drive_link"
+MODEL_PATH = "best_modelnew.h5.keras"
+GOOGLE_DRIVE_URL = "https://drive.google.com/uc?id=1nB_Sr_jnm0HmMSC4ISf2bFOYPGLW15Bc"
 EMOTION_LABELS = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 EMOTION_COLORS = {
     'angry': (0, 0, 255),      # Red
